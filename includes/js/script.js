db8f0bd4fdb115fe94244a642a8dc504b6bd9135
kalei <kalei>
2016-07-05 20:48:08 -0700

    init

---
+++ includes/js/script.js
@@ -0,0 +1,21 @@
+/*
+
+My Custom JS
+============
+
+Author:  C K Ashby
+Updated: August 2015
+Notes:	 
+
+*/
+
+$(function () {
+
+	$('#alertMe').click(function(e){
+		e.preventDefault();
+
+		$('#successAlert').slideDown();
+
+	});
+
+});
